7e3e0833e48c9cc4b03f6af529b364dcd21c53b3
Lasen <ltw21@ic.ac.uk>
2024-03-05 11:01:39 +0000

    Mass flow rate

---
+++ massflowrate.m
@@ -0,0 +1,22 @@
+function m_dot_a = m_dot_a(M_1, gamma, R, T_1, P_1, A_1)
+% Funtion to calculate the cycle efficiency using the compression
+% temperature ratio T1/T2
+%
+% Inputs:
+% M_1       = Mach number at inlet
+% gamma     = Ratio of specific heats
+% R         = Gas constant
+% T_1       = Temperature at inlet
+% P_1       = Freestream pressure
+% A_1       = Inlet Area
+
+% Outputs:
+% m_dot_a = mass flow rate
+
+U_1 = M_1*sqrt(gamma*R*T_1);
+
+rho_1 = P_1/(R*T_1);
+
+m_dot_a = rho_1*U_1*A_1;
+
+end
