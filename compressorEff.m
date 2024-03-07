aacb16ceec4dd56d9c3e207fba55e57efc3ae1a2
Wesley Hsu <wesley.hsu21@imperial.ac.uk>
2024-03-07 14:00:45 +0000

    some changes

--- compressorEff.m
+++ compressorEff.m
@@ -1,32 +1,9 @@
-% <<<<<<< Updated upstream
-% function comp_eff = compressorEff(P_1,P_2,gamma,T_1,T_2)
-% %function to calculate compressor efficiency from the inlet and compression
-% %temperatures
-% 
-% 
-% 
-% T_A = T_2* ( (P_2/P_1) ^ (gamma / (gamma-1) ) );
-% 
-% comp_eff = (T_2 - T_A)/(T_2 - T_1);
-% 
-% ||||||| ancestor
-% function comp_eff = compressorEff(P_1,P_2,gamma,T_1,T_2)
-% %function to calculate compressor efficiency from the inlet and compression
-% %temperatures
-% 
-% 
-% 
-% T_A = T_2* ( (P_2/P_1) ^ (gamma / (gamma-1) ) );
-% 
-% comp_eff = (T_2 - T_A)/(T_2 - T_1);
-% 
-% =======
 function comp_eff = compressorEff(P_1,P_2,gamma,T_1,T_2)
 %function to calculate compressor efficiency from the inlet and compression
 %temperatures
 
-
-
-T_A = T_2* ( (P_2/P_1) ^ (-(gamma-1)/gamma ) );
+T_A = T_2 * ( (P_2 / P_1) ^ ( -(gamma - 1) /gamma));
 
 comp_eff = (T_2 - T_A)/(T_2 - T_1);
+
+end
--- cycleEfficiency.m
+++ cycleEfficiency.m
@@ -1,4 +1,4 @@
-function cycle_eff = cycleEfficiency(T_1,T_2,f_fa,epsilon,comp_eff,exp_eff,C_p)
+function cycle_eff = cycleEfficiency(T_b,T_4,P_1,P_3,gamma,P_2,T_2,C_p,f_fa,epsilon)
 % Funtion to calculate the cycle efficiency using the compression
 % temperature ratio T1/T2
 %
@@ -17,10 +17,14 @@ function cycle_eff = cycleEfficiency(T_1,T_2,f_fa,epsilon,comp_eff,exp_eff,C_p)
 % fuel to air ratio f_fa and expansion and compression efficiencies
 % comp_eff exp_eff
 
-comp_T_ratio = T_1/T_2;
+% comp_T_ratio = T_1/T_2;
+% 
+% cycle_eff = (C_p *T_1/(f_fa * epsilon)) * (1/comp_T_ratio - 1) * (comp_eff * ...
+%     exp_eff * (1 + comp_T_ratio * f_fa * epsilon/ C_p * T_1) - 1);
 
-cycle_eff = (C_p *T_1/(f_fa * epsilon)) * (1/comp_T_ratio - 1) * (comp_eff * ...
-    exp_eff * (1 + comp_T_ratio * f_fa * epsilon/ C_p * T_1) - 1);
+exp_eff = expansionEff(T_b,T_4,P_1,P_3,gamma);
+comp_eff = compressorEff(P_1,P_2,gamma,T_1,T_2);
 
+cycle_eff = (C_p * T_1) / (f_fa * epsilon) * (T_2 / T_1 - 1) * (comp_eff * exp_eff * (1 + (T_1 / T_2) * ((f_fa * epsilon) / (C_p * T_1))) - 1);
 end
 
