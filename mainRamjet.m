68534bfb46e472ce788516de246f8aecb6215f72
Wesley Hsu <wesley.hsu21@imperial.ac.uk>
2024-03-10 19:02:42 +0000

    f

--- mainRamjet.m
+++ mainRamjet.m
@@ -1,4 +1,6 @@
-function [eta_thermo,eta_prop,eta_total,A_1,A_C1,A_2,A_b,A_C2,A_4] = mainRamjet(F,gamma,M_1,M_2,M_N,P_1,R,T_1,T_b,Pb_over_P2,P4_over_P1)
+function...
+[eta_thermo,eta_prop,eta_total,A_1,A_C1,A_2,A_b,A_C2,A_4,check_M_b_imag]...
+   = mainRamjet(F,gamma,M_1,M_2,M_N,P_1,R,T_1,T_b,Pb_over_P2,P4_over_P1)
 % Function to calculate the main output parameters of a ramjet given
 % relevant input parameters
 %
@@ -25,6 +27,7 @@ function [eta_thermo,eta_prop,eta_total,A_1,A_C1,A_2,A_b,A_C2,A_4] = mainRamjet(
 % A_b           = Area at the end of the burner         [m^2]
 % A_C2          = Second nozzle throat area             [m^2]
 % A_4           = Exhaust nozzle area                   [m^2]
+% check_M_b_imag= Checks if M_b is imaginary
 
 %% Station 1 - Freestream
 T01_over_T1 = M2T0ratio(M_1,gamma);
@@ -62,7 +65,8 @@ P0y_over_Py = M2P0ratio(M_y,gamma);
 
 P_y = P_1 * (2 * gamma * M_x^2 - (gamma - 1) ) / (gamma + 1);
 
-P_2 = P_y * ( ((1 + (gamma - 1)*M_y^2)/2 )^(gamma/(gamma-1))  ) / ((1 +  ((gamma -1)/2)*M_2^2  )^(gamma/(gamma - 1)));
+P_2 = P_y * ( ((1 + (gamma - 1)*M_y^2)/2 )^(gamma/(gamma-1))  ) /...
+    ((1 +  ((gamma -1)/2)*M_2^2  )^(gamma/(gamma - 1)));
 
 P_b = P_2 * 1; % Pb_over_P2;
 
@@ -81,7 +85,17 @@ T_2 = T2_over_T1 * T_1;
 
 P02_over_P2 = M2P0ratio(M_2,gamma);
 
-M_b = 0.5 * ((T_2/T_b)^0.5) * (M_2 + 1/(gamma*M_2)) - (0.5 * ( (T_2/T_b) * ((M_2 + 1/(gamma*M_2)) ^ 2) - 4/gamma)^0.5);
+% The following equation is derived from conservation of mass, momentum,
+% ideal gas law.
+% To find M_b, the subsonic root must be chosen.
+% When (0.5 * ( (T_2/T_b) * ((M_2 + 1/(gamma*M_2)) ^ 2) - 4/gamma)) is
+% negative, M_b becomes nonreal.
+M_b = 0.5 * ((T_2/T_b)^0.5) * (M_2 + 1/(gamma*M_2))...
+    - (0.5 * ( (T_2/T_b) * ((M_2 + 1/(gamma*M_2)) ^ 2) - 4/gamma)^0.5);
+
+% Checking imaginary:
+check_M_b_imag = isimag(...
+    0.5 * ( (T_2/T_b) * ((M_2 + 1/(gamma*M_2)) ^ 2) - 4/gamma));
 
 %% Station b - End of burner (burn complete)
 % Needs to be subsonic or flow will choke, one other constraint, see eq
@@ -126,11 +140,13 @@ T04_over_T0b = 1;
 T4_over_Tb = T0b_over_Tb * T04_over_T0b * (1 / T04_over_T4);
 T_4 = T_b * T4_over_Tb;
 
+% Neglecting drag and effect of fuel addition
 F_over_P1A1 = gamma * M_1^2 * (M_4^2 / M_1^2 * A4_over_A1 - 1);
 
 %% Outputs
 % Inlet area
-A_1 = 1 / (((P_1 * gamma * M_1^2) / F) * (((M_4 / M_1) ^ 2) * A4_over_A1 - 1));
+A_1 = 1 /...
+    (((P_1 * gamma * M_1^2) / F) * (((M_4 / M_1) ^ 2) * A4_over_A1 - 1));
 
 % Inlet throat area
 A_C1 = AC1_over_A1 * A_1;
@@ -147,7 +163,7 @@ A_C2 = AC2_over_A1 * A_1;
 % Exhaust area
 A_4 = A4_over_A1 * A_1;
 
-% Propulsive efficiency
+% Propulsive efficiency - assuming no fuel mass flow
 eta_prop = propEfficiency(F_over_P1A1,gamma,R,M_1,M_4,T_1,T_4);
 
 % Thermodynamic efficiency
